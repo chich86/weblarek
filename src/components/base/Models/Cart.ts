6f5f03dfcc0c6cf13eaee3b5c22fb8edb6c3519b
Daniil Grachev <daniilgracev765@yandex.ru>
2025-11-17 17:48:35 +0300

    Сделал классы моделей

---
+++ src/components/base/Models/Cart.ts
@@ -0,0 +1,39 @@
+import { IProduct } from "../../../types";
+
+export class Cart {
+  private items: IProduct[] = [];
+
+  getItems(): IProduct[] {
+    return this.items;
+  }
+
+  hasItem(productId: string): boolean {
+    return this.items.some((item) => item.id === productId);
+  }
+
+  addItem(product: IProduct): void {
+    if (!this.hasItem(product.id)) {
+      this.items.push(product);
+    }
+  }
+
+  removeItem(itemId: string): void {
+    this.items = this.items.filter(item => item.id !== itemId);
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  getCount(): number {
+    return this.items.length;
+  }
+
+  getTotal(): number {
+    return this.items.reduce((total, item) => total + (item.price ?? 0), 0);
+  }
+
+  get totalPrice(): number {
+    return this.items.reduce((total, item) => total + (item.price ?? 0), 0);
+  }
+}
